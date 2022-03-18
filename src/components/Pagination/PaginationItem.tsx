87a544ba28212a23823f0fc0263001c62a114447
Matheus Ramos Kafuri <matheuskafuri@Matheuss-MacBook-Pro.local>
2022-03-18 15:32:46 -0300

    react-query implementation

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,9 +3,10 @@ import { Button } from "@chakra-ui/react";
 interface PaginationProps {
     number: number;
     isCurrent?: boolean;
+    onPageChange: (page: number) => void;
 }
 
-export function PaginationItem({ isCurrent = false, number }: PaginationProps) {
+export function PaginationItem({ isCurrent = false, number, onPageChange }: PaginationProps) {
     if (isCurrent) {
         return (
             <Button size="sm" fontSize="xs" width="4" colorScheme="pink" disabled _disabled={{ bg: "pink.500", cursor: "default" }}>
@@ -14,7 +15,17 @@ export function PaginationItem({ isCurrent = false, number }: PaginationProps) {
         );
     }
     return (
-        <Button size="sm" fontSize="xs" width="4" bg="gray.700" _hover={{ bg: "gray.500" }}>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bg="gray.700"
+          _hover={{
+            bg:
+            "gray.500"
+          }}
+          onClick={() => onPageChange(number)}
+        >
             {number}
         </Button>
     )
